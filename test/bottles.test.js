4ab21b43168bc35da36db41c24a0aa057b825cae
Tom Stuart <tom@codon.com>
2019-04-24 23:53:10 +0100

    Delete redundant test

--- test/bottles.test.js
+++ test/bottles.test.js
@@ -1,15 +1,6 @@
 import { Bottles, BottleVerse } from '../lib/bottles';
 
 describe('Bottles', () => {
-  test('the first verse', () => {
-    const expected =
-      '99 bottles of milk on the wall, ' +
-      '99 bottles of milk.\n' +
-      'Take one down and pass it around, ' +
-      '98 bottles of milk on the wall.\n';
-    expect(new Bottles().verse(99)).toBe(expected);
-  });
-
   test('another verse', () => {
     const expected =
       '3 bottles of milk on the wall, ' +
